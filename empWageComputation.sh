12df6d687fbd442203fd658611fe8258125f5d8d
Naveen2706 <naveen271996@gmail.com>
2021-06-03 19:11:54 +0530

    wage till total working hour or month is reached

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -13,18 +13,26 @@ esac
 fulldayhour=8
 parttimehour=4
 wagephour=20
+month=0
+totalhr=0
+totalwage=0
+
+while [[ $month != 21 ]];do
 
 W=$((4+((4*$((RANDOM%2))))))
 case "$W" in
 "8")
 echo wage for fullday: "$(($fulldayhour*$wagephour))"
-echo wage of a month "$((20* $(($fulldayhour*$wagephour))))"
-
+totalhr=$(($totalhr+$W))
+((month++))
 ;;
+
 "4")
 echo part time wage  : "$(($parttimehour*$wagephour))"
-echo wage of a month "$((20* $(($parttimehour*$wagephour))))"
-
+totalhr=$(($totalhr+$W))
+((month++))
 ;;
 esac
-
+done
+totalwage=$(($totalhr*20))
+echo wage for working for a month:"$totalwage"
