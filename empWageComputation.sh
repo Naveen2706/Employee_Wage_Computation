26bc1eb3a4f2faa1f9a54197280f1b83de237232
Naveen2706 <naveen271996@gmail.com>
2021-06-04 21:45:19 +0530

    total working hours or days condition

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,13 @@
 Welcome to employee wage computation
 
-declare -A dandtwage
-declare -A Day
-
 x=$((RANDOM%2))
 case "$x" in
-   "1")
-	echo employee is present
-   ;;
-   "0")
-	echo employee is absent
-   ;;
+"1")
+		echo employee is present
+;;
+"0")
+		echo employee is absent
+;;
 esac
 
 fulldayhour=8
@@ -19,67 +16,29 @@ wagephour=20
 month=1
 totalhr=0
 totalwage=0
-d=0
-dailywage=0
-check=0
 
-function workhours
-{
 while [[ $month != 21 ]];do
 
-   W=$((4+((4*$((RANDOM%2))))))
-
-   check=$(($month%5))
-
-   case "$check" in
-      "1")
-		Day[$month]="Monday"
- 		echo "Monday"
-       ;;
-      "2")
-		Day[$month]="Tuesday"
-        	echo "Tuesday"
-       ;;
-      "3")
-		Day[$month]="Wednusday"
-		echo "Wednusday"
-       ;;
-      "4")
-		Day[$month]="Thursday"
-		echo "Thursday"
-       ;;
-      "0")
-		Day[$month]="Friday"
-		echo "Friday"
-       ;;
-   esac
-
-
-    case "$W" in
-       "8")
-    		dailywage=$(($W*20))
-    		totalhr=$(($totalhr+$W))
-    		dandtwage[$d]=$dailywage
-		echo dailywage :${dandtwage[$d]}
-		((d++))
-		((month++))
-	;;
-
-	"4")
-		dailywage=$(($W*20))
+W=$((4+((4*$((RANDOM%2))))))
+case "$W" in
+"8")
+		echo wage for fullday: "$(($fulldayhour*$wagephour))"
 		totalhr=$(($totalhr+$W))
-		dandtwage[$d]=$dailywage
-		echo dailywage :${dandtwage[$d]}
-		((d++))
 		((month++))
- 	;;
-    esac
-    done
+;;
 
- echo total work hours are "$totalhr"
- totalwage=$(($totalhr*20))
- dandtwage[$d]=$totalwage
- echo total wage : ${dandtwage[$d]}
-}
+"4")
+		echo part time wage  : "$(($parttimehour*$wagephour))"
+		totalhr=$(($totalhr+$W))
+		((month++))
+;;
+esac
+if [[ $totalhr -ge 100 ]];then
+		echo 100 hours limit reached
+		break
+fi
 
-workhours
+done
+
+totalwage=$(($totalhr*20))
+echo wage for working for a month:"$totalwage"
